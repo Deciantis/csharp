94d8091ea508774f91acfe331260e60fb606dfac
D. Brutus <7785171+Deciantis@users.noreply.github.com>
2020-04-17 22:35:44 -0400

    added logic to follow, pseudo code may not properly reflect correct formula

--- Program.cs
+++ Program.cs
@@ -1,12 +1,44 @@
-﻿using System;
+﻿//This program checks if a number is divisible by 7
+
+
+//1) read in string
+//2) convert to char[n]
+//3) double last digit [(n-1)*2]
+//4) convert remaining to int [(n-1)] -- This logic is flawed, I'm pretty sure of it. Moving on. Fix later.
+//4) subtract it to the remaining number
+
+
+//Walk through
+//1) 203
+//2) [2][0][3]
+//3) [3]*2 = 6
+//4) [2][0] = 20
+//5) 20-6 = 13
+//6) 13 mod 7 = 6
+//7) not divisible ( 13 mod 7 must equal 0)
+using System;
 
 namespace div_by_7
 {
     class Program
     {
-        static void Main(string[] args)
+         void Main(String[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter an integer");
+            doDiv(args[0]);
         }
+
+        private int doDiv (string n){
+        char[] t;
+        t=n.ToCharArray();
+
+        int x=0;        
+        return x;
     }
+    }
+
+
+
+
+
 }
--- div_by_7.cs
+++
@@ -1,9 +0,0 @@
-//Div by 7 algo
-using System;
-
-namespace HelloWOrld
-{
-    static void Main(){
-    console.WriteLine("Hello World");
-}
-}
